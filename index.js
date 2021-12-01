2734ec4c0d06a309e9e3f304fab46794ecc1190a
villysiu <villysiu@gmail.com>
2021-11-30 22:33:01 -0800

    commit tree c17d78b7015139cd0e05545f6864b2986dc250be

---
+++ index.js
@@ -0,0 +1,19 @@
+class Scooter {
+    constructor(year, color, model) {
+        this.year = year
+        this.color = color
+        this.model = model
+    }
+}
+
+function Driver(name, age, experience){
+
+        this.name = name,
+        this.age = age,
+        this.experience = experience
+   
+}
+function PickupLocation(address, city) {
+    this.address = address, 
+    this.city = city
+}
